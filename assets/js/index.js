be92476e69cbb7c088200bb66dc2fe9d7d2abe0b
John <john@Johns-MacBook-Pro.local>
2018-04-06 17:57:42 -0400

    moved sticky nav animation trigger up

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     document.getElementById("sticky-nav").style.top = "0";
   } else {
     document.getElementById("sticky-nav").style.top = "-80px";
